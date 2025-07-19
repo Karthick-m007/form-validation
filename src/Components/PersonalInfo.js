8e949979141d14df9aeeeedbf4f209a04e0e283c
Karthick-m007 <karthickraja200211@gmail.com>
2025-07-19 10:50:55 +0530

    first commit

---
+++ src/Components/PersonalInfo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function PersonalInfo() {
+  return (
+    <div className=''>PersonalInfo
+
+
+        <div className='border  w-96 mx-auto '>
+            <h2 className='flex justify-content-center'>Personal Information Form</h2>
+        </div>
+    </div>
+  )
+}
